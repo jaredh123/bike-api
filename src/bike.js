191a2bbc5a0c9b4eec98059d065b48e08868d014
Jared Hanson and Olha Wysocky <jared@>
2019-03-28 10:16:14 -0700

    Add more input values and submission forms

--- src/bike.js
+++ src/bike.js
@@ -1,22 +1,45 @@
 import $ from 'jquery';
 
-export
+export class BikeService {
+  getBikesByCity(location, count) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=${count}&query=Seattle&location=${location}&distance=10&stolenness=stolen`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 
-export function template(){
-  $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&query=${color}`).then(function(response)
-  {
+  printLocation(response){
     let location = "";
-    let title = "";
-    let year = "";
     for (let i = 0; i < response.bikes.length; i++) {
       location += "<li>" + response.bikes[i].stolen_location + "</li><br>";
+    }
+    return location;
+  }
+
+
+  printTitle(response){
+    let title = "";
+    for (let i = 0; i < response.bikes.length; i++) {
       title += "<li>" + response.bikes[i].title + "</li><br>";
+    }
+    return title;
+  }
+
+
+  printYear(response){
+    let year = "";
+    for (let i = 0; i < response.bikes.length; i++) {
       year += "<li>" + response.bikes[i].year + "</li><br>";
     }
-  }).fail(function(error)
-  {
-  let error = (`There was an error processing your request: ${error.responseText}. Please try again.`);
-  });
-
-  return location, title, year, error;
+    return year;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -3,31 +3,44 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+import {BikeService} from "./bike"
+
 $(document).ready(function() {
-  $('#bike-form').submit(function(event) {
+  $('#color-form').submit(function(event) {
     event.preventDefault();
     let color = $('#color').val();
+    let count = $('#count').val();
 
-    console.log(color);
-
-    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen&query=${color}`).then(function(response)
+    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=${count}&location=IP&distance=10&stolenness=stolen&query=${color}`).then(function(response)
     {
-      let location = "";
-      let title = "";
-      let year = "";
-      for (let i = 0; i < response.bikes.length; i++) {
-        location += "<li>" + response.bikes[i].stolen_location + "</li><br>";
-        title += "<li>" + response.bikes[i].title + "</li><br>";
-        year += "<li>" + response.bikes[i].year + "</li><br>";
-      }
-      $('.location').append(location);
-      $('.title').append(title);
-      $('.year').append(year);
-
-
+      let bikeService = new BikeService();
+      $('.location').append(bikeService.printLocation(response));
+      $('.title').append(bikeService.printTitle(response));
+      $('.year').append(bikeService.printYear(response));
     }).fail(function(error)
     {
       $('.color').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
   });
+  // =========================================
+
+  $('#location-form').submit(function(event) {
+    event.preventDefault();
+    let location = $('#location').val();
+    let count = $('#count').val();
+
+    let bikeService = new BikeService();
+    let promise = bikeService.getBikesByCity(location, count);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let bikeService = new BikeService();
+      $('.location').append(bikeService.printLocation(body));
+      $('.title').append(bikeService.printTitle(body));
+      $('.year').append(bikeService.printYear(body));
+    }, function(error) {
+      $('.error').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
 });
